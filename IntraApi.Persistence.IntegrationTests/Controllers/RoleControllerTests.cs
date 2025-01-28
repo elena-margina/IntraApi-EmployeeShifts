56adf4657b2a76a08573739a203be37342288119
elena-margina <elena.margiina@gmail.com>
2025-01-28 13:08:38 +0200

    [update]

---
+++ IntraApi.Persistence.IntegrationTests/Controllers/RoleControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using IntraApi.API.IntegrationTests.Base;
+using IntraApi.Application.Features.Roles.Queries.GetRolesList;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Text.Json;
+
+namespace IntraApi.API.IntegrationTests.Controllers
+{
+    public class RoleControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public RoleControllerTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/Role/GetAllRoles");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<RoleListVm>>(responseString);
+
+            Assert.IsType<List<RoleListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
